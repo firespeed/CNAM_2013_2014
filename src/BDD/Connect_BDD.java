57e0b6b5970d15714f9cd7851592a14e6778f583
rewiko <anthony.comtois@gmail.com>
2013-11-16 10:06:55 +0100

    [host machine] update to work fine on host machine

--- src/BDD/Connect_BDD.java
+++ src/BDD/Connect_BDD.java
@@ -10,7 +10,7 @@ public class Connect_BDD {
 	private static String url = "jdbc:mysql://localhost:3306/vod";    
     private static String driverName = "com.mysql.jdbc.Driver";   
     private static String username = "root";   
-    private static String password = "";
+    private static String password = "root";
     private static Connection con;
 
 
